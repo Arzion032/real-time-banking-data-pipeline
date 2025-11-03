67a243b9bc8f3b3fdadae700c9748c95292387e6
Arzion032 <jmelvinsarabia032@gmail.com>
2025-11-03 15:47:02 +0800

    finalized staging models

--- bank/models/staging/stg_customers.sql
+++ bank/models/staging/stg_customers.sql
@@ -6,13 +6,17 @@ WITH ranked AS (
         v:first_name::string AS first_name,
         v:last_name::string AS last_name,
         v:email::string AS email,
+        v:phone::string AS phone,
+        v:date_of_birth::date AS date_of_birth,
+        v:address::string AS cust_address,
+        v:status::string AS cust_status,
         v:created_at::timestamp AS created_at,
-        current_timestamp AS load_timestamp,
+        CURRENT_TIMESTAMP AS load_timestamp,
         ROW_NUMBER() OVER (
             PARTITION BY v:id::string 
             ORDER BY v:created_at::timestamp DESC
             ) AS rn
-    FROM {{ source('raw','customers') }} v
+    FROM {{ source('raw','customers') }}
 )
 
 SELECT
@@ -20,7 +24,11 @@ SELECT
     first_name,
     last_name,
     email,
+    phone,
+    date_of_birth,
+    cust_address,
+    cust_status,
     created_at,
     load_timestamp
 FROM ranked
-WHERE rn = 1;
+WHERE rn = 1
